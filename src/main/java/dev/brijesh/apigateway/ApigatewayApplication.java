71da82e3b3531d6a66e57c2f54814dffbde30b80
Brijesh Kushwaha <bkushwaha89@gmail.com>
2024-02-27 16:18:28 +0530

    apigateway first commit

---
+++ src/main/java/dev/brijesh/apigateway/ApigatewayApplication.java
@@ -0,0 +1,13 @@
+package dev.brijesh.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayApplication.class, args);
+    }
+
+}
